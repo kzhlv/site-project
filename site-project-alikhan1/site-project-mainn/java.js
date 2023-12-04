aff9551db69bad35dc9c2964076773bc1f532a61
alikhan2281337 <89764674+alikhan2281337@users.noreply.github.com>
2023-12-04 10:27:08 +0600

    FINAL IS GOING TO BE PASSED

---
+++ site-project-alikhan1/site-project-mainn/java.js
@@ -0,0 +1,14 @@
+let isAuth = false
+
+let urlSites = document.querySelectorAll('.url')
+console.log(urlSites)
+
+urlSites.forEach(element => {
+    element.addEventListener('click', (event) => {
+        if (!isAuth) {
+            event.preventDefault();
+            window.location.href = '/site-project-main2/registration2.html';
+        }  
+            
+    })
+});
